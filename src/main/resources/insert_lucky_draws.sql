d09e3904da351f9be877b400ec3174d097a74714
JIN Mengting <mengting.jin.ext@tomoloyalty.com>
2023-03-30 02:31:16 +0800

    [3] create lucky draw, prize, and lucky draw prize table

---
+++ src/main/resources/insert_lucky_draws.sql
@@ -0,0 +1,6 @@
+INSERT INTO lucky_draw (name, description, max_entries) VALUES
+      ('Lucky draw A', 'Get your prizes now!', 100),
+       ('Lucky draw B', 'Get your prizes now!', 50),
+       ('Lucky draw C', 'Get your prizes now!', 20),
+       ('Lucky draw D', 'Get your prizes now!', 30),
+       ('Lucky draw E', 'Get your prizes now!', 200);
---
+++ src/main/resources/insert_prizes.sql
@@ -0,0 +1,6 @@
+INSERT INTO prize (name, description, value, stock) VALUES
+      ('Coca Cola', '250 ml/can', '$2', 5),
+      ('Spirits', '250 ml/can', '$2', 5),
+      ('Pepsi', '250 ml/can', '$2', 5),
+      ('Oven', 'Electric Oven of 42L', '$200', 2),
+      ('Iphone 14', 'The newest iphone with storage of 256GB', '$1500', 1);
