5a389833508565f11b820e1c6388193ed513c1d8
JIN Mengting <mengting.jin.ext@tomoloyalty.com>
2023-05-22 00:41:48 +0800

    [14] fix lucky draw mode name problem

--- src/main/resources/db/lucky_draw/insert_lucky_draws.sql
+++ src/main/resources/db/lucky_draw/insert_lucky_draws.sql
@@ -1,5 +1,5 @@
 INSERT INTO lucky_draw (name, description, max_entries, mode, start_time, end_time) VALUES
-      ('Lucky draw A', 'Get your prizes now!', 100, 'BY_STOCK', '2023-05-21 00:00:00', '2023-5-31 16:00:00');
+      ('Lucky draw A', 'Get your prizes now!', 100, 'BYSTOCK', '2023-05-21 00:00:00', '2023-5-31 16:00:00');
 --        ('Lucky draw B', 'Get your prizes now!', 50),
 --        ('Lucky draw C', 'Get your prizes now!', 20),
 --        ('Lucky draw D', 'Get your prizes now!', 30),
